a3281cbaa532bf2ef3b2d376721b62aa0f25f6a4
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-05-27 10:52:49 -0700

    added api of metadata

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xe655A17F0F2F07C28B6f452b6f68A777F1614842";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-bay-nine.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
